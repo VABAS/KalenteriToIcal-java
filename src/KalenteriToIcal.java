18fd19337d79be01cd78d5c4adfbca972dd781b1
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-06-08 17:19:12 +0300

    Tehty mahdollisuus kurssilinkkien tuontiin tiedostosta

--- src/KalenteriToIcal.java
+++ src/KalenteriToIcal.java
@@ -1,10 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 class KalenteriToIcal {
     // Usage string.
-    static String usage = "KalenteriToIcal osoite tiedosto.ics [eimon] [eikys]";
+    static String usage = "KalenteriToIcal osoite tiedosto.ics [eimon] [eikys]"
+                          + "\nPakolliset parametrit ovat syöttö-"
+                          + "osoite/tiedosto sekä tulostustiedosto jotka ovat "
+                          + "aina ensimmäinen ja toinen parametri."
+                          + "\n\nLisäasetukset:"
+                          + "\n--privacy [PUBLIC|CONFIDENTIAL|PRIVATE]"
+                          + "\n  Määrittää kaikkien tapahtumien "
+                          + "yksityisyysarvon (ical CLASS-kenttä)."
+                          + "\n--fromfile"
+                          + "\n  Määrittää, että kurssiosoitteet haetaan "
+                          + "tiedostosta. Tiedoston sijainti kirjoitetaan "
+                          + "osoitteen paikalle.";
 
     public static void main (String args[]) {
         if (args.length < 2) {
@@ -18,6 +31,9 @@ class KalenteriToIcal {
         String file = args[1];
         // Privacy string. Defaults to confidential.
         String veventPrivacy = "CONFIDENTIAL";
+        
+        // Fromfile status.
+        boolean fromFile = false;
 
         // Initializing eimon and eikys statuses to false here.
         boolean eimon = false;
@@ -31,6 +47,9 @@ class KalenteriToIcal {
                     break;
                 case "eikys":
                     eikys = true;
+                    break;
+                case "--fromfile":
+                    fromFile = true;
                     break;
                 case "--privacy":
                     String privacy = "";
@@ -54,9 +73,42 @@ class KalenteriToIcal {
             }
         }
         
-        // Finding out the individual course timetable links.
-        String[] links = AsioParse.extractLinks(address, eikys);
+        //String[] links;
+        ArrayList<String> links = new ArrayList<String>();
         
+        // Deciding whether to fetch links from asio or from local file.
+        if (fromFile) {
+            System.out.println("Haetaan linkit tiedostosta '" + file + "'");
+            BufferedReader inFile = null;
+            try {
+                inFile = new BufferedReader(new FileReader(address));
+                String line;
+                while ((line = inFile.readLine()) != null) {
+                    if (line.length() <= 0) {
+                        continue;
+                    }
+                    links.add(line);
+                }
+            }
+            catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if (inFile != null) {
+                        inFile.close();
+                    }
+                }
+                catch (java.io.IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        else {
+            System.out.println("Haetaan linkit asiosta.");
+            // Finding out the individual course timetable links.
+            links = new ArrayList<>(Arrays.asList(AsioParse.extractLinks(address, eikys)));
+        }
         // Contructing vcalendar object. Now vevents here, adding them later.
         Vcalendar vcalendar = new Vcalendar("2.0", "sikkela");
         
